c52e43b6bcdcd79ba3bc49fe35727d0c269e3c5f
Jervie Manabat <jervie.manabat@student.ateneo.edu>
2025-01-14 15:26:54 +0800

    Update TrackerConsole.java

--- TrackerConsole.java
+++ TrackerConsole.java
@@ -1,24 +1,3 @@
-/**
-	This is the TrackerConsole class. 
-	It is the main class that accepts input until 
-	"Report" is entered that causes the break. 
-	The methods are called in this class.
-	@author Jervie S. Manabat (223961)
-	@version December 03, 2022
-**/
-
-/*
-	I have not discussed the Java language code in my program 
-	with anyone other than my instructor or the teaching assistants 
-	assigned to this course.
-	I have not used Java language code obtained from another student, 
-	or any other unauthorized source, either modified or unmodified.
-	If any Java language code or documentation used in my program 
-	was obtained from another source, such as a textbook or website, 
-	that has been clearly noted with a proper citation in the comments 
-	of my program.
-*/
-
 import java.util.*;
 public class TrackerConsole{
 	public static void main ( String[] args ){
