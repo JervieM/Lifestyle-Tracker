447df197b9079974230800edfe84d063250a3587
Jervie Manabat <jervie.manabat@student.ateneo.edu>
2025-01-14 15:27:06 +0800

    Update LifestyleTracker.java

--- LifestyleTracker.java
+++ LifestyleTracker.java
@@ -1,25 +1,3 @@
-/**
-	The LifestyleTracker class. 
-	Instances of food and activity are created here. 
-	Additionally, it has the eat and perform methods, 
-	which carry out all necessary operations. 
-	It can show a report of eaten foods and performed activities.
-	@author Jervie S. Manabat (223961)
-	@version December 03, 2022
-**/
-
-/*
-	I have not discussed the Java language code in my program 
-	with anyone other than my instructor or the teaching assistants 
-	assigned to this course.
-	I have not used Java language code obtained from another student, 
-	or any other unauthorized source, either modified or unmodified.
-	If any Java language code or documentation used in my program 
-	was obtained from another source, such as a textbook or website, 
-	that has been clearly noted with a proper citation in the comments 
-	of my program.
-*/
-
 import java.util.*;
 public class LifestyleTracker {
 	private ArrayList <Double> foodCal;
